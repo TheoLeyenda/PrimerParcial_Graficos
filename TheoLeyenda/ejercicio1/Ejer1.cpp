b8bb41180df6fe19114e8700e4fd46efeab15140
theixgamer <theofritaku@gmail.com>
2020-05-19 10:27:24 -0300

    Initial commit

---
+++ TheoLeyenda/ejercicio1/Ejer1.cpp
@@ -0,0 +1,10 @@
+#include "Ejer1.h"
+using namespace parcial;
+void Ejer1::MostrarCreditos()
+{
+	cout << "Theo Leyenda" << endl;
+}
+float Ejer1::Diferencia(float num1, float num2)
+{
+	return num1 - num2;
+}
---
+++ TheoLeyenda/ejercicio1/Ejer1.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+namespace parcial 
+{
+	class Ejer1
+	{
+	public:
+		void MostrarCreditos();
+		float Diferencia(float num1, float num2);
+	};
+}
